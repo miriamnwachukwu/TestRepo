72f12eaff4f139405fc245c6a1b8cf73046f9223
Miriam Nwachukwu <miriam.nwachukwu@uwaterloo.ca>
2019-09-14 13:36:52 -0400

    My first commit on GitHub

---
+++ Script.py
@@ -0,0 +1,17 @@
+greeting = 'Hello'
+name = 'Michael'
+
+message = greeting + ', ' + name + '. Welcome!'
+
+#message = f'{greeting}, {name}. Welcome!'
+
+print(f'The value of pi is approximately {name}.')
+
+print(message)
+
+# x = '''Bobby was a good kid
+# until he got to university'''
+
+# print(x)
+
+# print(help(str))
